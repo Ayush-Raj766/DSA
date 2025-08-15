35ae0b958ae3a735fcc79ab196e59552edee4791
Ayush Jaiswal <ayushjais766@gmail.com>
2025-08-15 10:20:32 +0530

    set up

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int a,b;
+    cin >> a >> b;
+    cout << "Sum: " << a + b << endl;
+    cout << "Difference: " << a - b << endl;
+    cout << "Hello, World!" << endl;
+    return 0;
+}
